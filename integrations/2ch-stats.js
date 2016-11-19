f3d650f5c64e1d71502f6a69680079768e0aef83
aemxdp <aemxdp@gmail.com>
2016-11-19 20:51:39 +0200

    Tune date parsing in 2ch-stats to handle saturdays properly

--- integrations/2ch-stats.js
+++ integrations/2ch-stats.js
@@ -5,7 +5,8 @@ module.exports = {
     lastPostTime: (document) => {
         moment.locale('ru');
         return moment.tz(
-            document.querySelector('.post-wrapper:last-child .posttime').textContent,
+            document.querySelector('.post-wrapper:last-child .posttime')
+                .textContent.replace('Суб', 'Сбт'),
             'DD/MM/YY ddd HH:mm:ss',
             'Europe/Moscow'
         ).toJSON();
