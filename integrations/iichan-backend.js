30c79b522a4145d1cc480f9c617a324d9ce16517
aemxdp <aemxdp@gmail.com>
2016-11-22 04:20:18 +0200

    Improve error handling for iichan integration

--- integrations/iichan-backend.js
+++ integrations/iichan-backend.js
@@ -4,7 +4,17 @@ module.exports = {
     uploadFiles: common.concatImagesUploader('input[type="file"]', 'jpeg'),
 
     submitPage: (page) => new Promise((resolve, reject) => {
-        page.on('onLoadFinished', resolve);
+        page.on('onLoadFinished', async () => {
+            let url = await page.property('url');
+            if (url.indexOf('iichan.hk/cgi-bin/wakaba.pl') != -1)
+                reject(new common.SubmitError(
+                    await page.evaluate(function () {
+                        return document.querySelector('p').textContent.trim();
+                    })
+                ));
+            else
+                resolve();
+        });
         common.submitPage(page).catch(reject);
     })
 };
